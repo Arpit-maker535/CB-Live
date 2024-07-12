f4c9f91816ed194a1cbe9a0bbb8ff6f0d449c311
Nirbhay Luthra <nirbhayluthra@gmail.com>
2024-07-12 23:16:31 +0530

    chat app

--- Class/apis.js
+++ Class/apis.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createUser } = require("./functions");
+const { createUser, createProduct, createOrder } = require("./functions");
 const router = express.Router();
 
 router.post("/users", async (req, res) => {
@@ -10,6 +10,47 @@ router.post("/users", async (req, res) => {
   } catch (e) {
     console.log("Error in creating user", e);
     res.status(500);
+  }
+});
+
+router.post("/products", async (req, res) => {
+  try {
+    const { name, description, price } = req.body;
+
+    const products = await createProduct(name, description, price);
+    return res.json(products);
+  } catch (e) {
+    console.log("Error in creating products", e);
+    res.status(500);
+  }
+});
+router.post("/orders", async (req, res) => {
+  try {
+    const { user_id, product_id, quantity } = req.body;
+    const orders = await createOrder(user_id, product_id, quantity);
+    res.json(orders);
+  } catch (e) {
+    console.log("Error in creating orders", e);
+    res.status(500);
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  try {
+  } catch (e) {
+    console.log("Error in fetching orders", e);
+    res.status(500);
+  }
+});
+
+router.put("/users/:id", async (req, res) => {
+  try {
+    const updates = req.body;
+    const user = await updateUser(req.params.id, updates);
+    res.json(user);
+  } catch (error) {
+    console.error("Error updating user:", error.stack);
+    res.status(500).send("Error updating user");
   }
 });
 
--- Class/functions.js
+++ Class/functions.js
@@ -6,6 +6,74 @@ const createUser = async (name, email) => {
   return await User.create({ name, email });
 };
 
+const createProduct = async (name, description, price) => {
+  return await Product.create({ name, description, price });
+};
+
+const createOrder = async (user_id, product_id, quantity) => {
+  return await Order.create({ user_id, product_id, quantity });
+};
+
+const getUserById = async (id) => {
+  return await User.findAll(id);
+};
+
+const getAllUsers = async () => {
+  return await User.findAll();
+};
+
+const getAllProducts = async () => {
+  return await Product.findAll();
+};
+
+const getAllOrders = async () => {
+  return await Order.findAll({
+    include: [
+      { model: User, attributes: ["name", "email"] },
+      {
+        model: Product,
+        attributes: ["name", "price"],
+      },
+    ],
+  });
+};
+
+const getOrderDetails = async () => {
+  return await Order.findOne({
+    where: { id: order_id },
+  });
+};
+
+const editAProduct = async (id, name, description, price) => {
+  const product = await Product.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (product) {
+    product.name = name;
+    product.description = description;
+    product.price = price;
+    product.save();
+  }
+  return product;
+};
+
+const updateUser = async (id, updates) => {
+  const product = await Product.findByPk(id);
+  if (product) {
+    return await product.update();
+  }
+};
+
 module.exports = {
   createUser,
+  createProduct,
+  createOrder,
+  getUserById,
+  getAllUsers,
+  getAllProducts,
+  getAllOrders,
+  getOrderDetails,
 };
